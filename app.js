4fa14175afb32fa8fe869e325a42c72cdae02596
guslopezr <guslopezr@gmail.com>
2023-01-30 08:45:23 -0300

    cambios en el puerto

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* 
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -9,4 +10,21 @@ app.use(express.json())
 app.use(express.static('public'))
 
 app.use('/', indexRoutes)
-app.listen(PORT, console.log(`SERVER ON PORT: ${PORT}`))
+app.listen(PORT, console.log(`SERVER ON PORT: ${PORT}`)) */
+
+///
+
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const indexRoutes = require('./src/routes/indexRoutes')
+const port = process.env.PORT || 6787;
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public'))
+
+app.use('/', indexRoutes)
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
