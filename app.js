35c70b1bde34c46ac7f4a68a90427b3c070ef3c3
guslopezr <guslopezr@gmail.com>
2023-01-31 21:22:52 -0300

    ajustes en app.js

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const indexRoutes = require('./src/routes/indexRoutes')
-const PORT = process.env.PORT || '3000';
+    //const PORT = process.env.PORT || '3000';
+
+const PORT = process.env.APP_PORT;
+
 
 app.use(cors())
 app.use(express.json())
