0016954c683fab510ae23ba824e3c8fb8d47e4f4
guslopezr <guslopezr@gmail.com>
2023-02-02 18:25:24 -0300

    agregando dotenv

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ const cors = require('cors')
 const app = express();
 const indexRoutes = require('./src/routes/indexRoutes')
 require('dotenv').config()
-
 const PORT = process.env.PORT || 3000;
 //const PORT = 5040;
 
